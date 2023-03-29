c8db035bd3e73c98c813d304e4e7146253e62582
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-03-29 23:10:54 +0530

    Added icons for github and google login and controller functions for same

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,11 +1,15 @@
 import { SetStateAction, useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
 import axios from "axios";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 import Input from "@/components/Input";
 
 const Auth = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
@@ -26,14 +30,13 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-      router.push("/");
+      // router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   // Register user with credentials
   const register = useCallback(async () => {
@@ -91,6 +94,20 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign Up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
