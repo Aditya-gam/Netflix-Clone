3563d098c6a197e5b1db926cb38c9144472089ea
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-04-06 12:18:54 +0530

    Added movie list component to main page

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -4,6 +4,8 @@ import { getSession } from "next-auth/react";
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,6 +26,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   // const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
@@ -34,6 +37,9 @@ export default function Home() {
       </button> */}
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
